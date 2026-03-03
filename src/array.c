637380ca4adda1a19c366b58303b40e47a31b3c6
Jonathan Ebel <abendmal-farn.77@icloud.com>
2026-03-03 21:30:24 +0100

    array.c/.h setup

---
+++ src/array.c
@@ -0,0 +1,17 @@
+#include "array.h"
+#include <stdio.h>
+
+//Fill the given array at pointer a with n random nummbers
+void fill_array(int n, int *a){
+    for(int i = 0 ; i<n; i++){
+        a[i] = rand() % 10;
+    }
+}
+
+//Creates and returns a pointer to array a length n with random numbers
+int *create_array(int n){
+    int *array = malloc(sizeof(int) * n);
+    fill_array(n, array);
+
+    return array;
+}
---
+++ src/array.h
@@ -0,0 +1,11 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+//Fill the given array at pointer a with n random nummbers
+void fill_array(int n, int *a);
+
+//Creates and returns a pointer to array a length n with random numbers
+int *create_array(int n);
+
+
+#endif
